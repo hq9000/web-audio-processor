415fc63afc4c8740865d8a8d1973f85234bb5f0e
Sergey <grechin.sergey@gmail.com>
2019-12-30 10:38:10 +0300

    parsing CLI args in dsp processing script

--- dsp/process.py
+++ dsp/process.py
@@ -1,4 +1,12 @@
 import argparse
+import sndhdr
+import os
+
+
+def fail_with_error(error_message):
+    print(error_message)
+    exit(1)
+
 
 parser = argparse.ArgumentParser()
 
@@ -11,3 +19,14 @@ args = parser.parse_args()
 source_file = args.source_file
 destination_file = args.destination_file
 api_root = args.api_root
+
+if not os.path.isfile(source_file):
+    fail_with_error('source file ' + source_file + ' does not exist')
+
+source_file_info = sndhdr.what(source_file)
+
+if source_file_info is None:
+    fail_with_error('unrecognizable sound file format')
+
+if source_file_info.filetype != 'wav':
+    fail_with_error('only wav files are supported. Provided: ' + source_file_info.filetype)
